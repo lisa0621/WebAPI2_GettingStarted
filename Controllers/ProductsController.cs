d788658f042de708919bfc463113e0a54943237c
SHA <lisa0621@gmail.com>
2016-08-29 08:38:32 +0800

    Calling the Web API with Javascript and jQuery

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PorductsApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +10,26 @@ namespace PorductsApp.Controllers
 {
     public class ProductsController : ApiController
     {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
     }
 }
